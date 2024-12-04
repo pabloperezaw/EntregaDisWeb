34331891f398d8d0175f778e906abe57fb70dbbe
pabloperezaw <78221385+pabloperezaw@users.noreply.github.com>
2024-12-04 21:39:44 +0100

    Add files via upload

---
+++ Práctica Postal Navideña/EJ14. Navidad/script.js
@@ -0,0 +1,8 @@
+// El archivo js...
+// Así os ahorro el gran trabajo y sudor que supone crearlo y así Miguel está obligado a no integrarlo en el HTML
+var hoho = true;
+
+function elem_visibles(hoho){
+    const santa = document.querySelector('.santa');
+    santa.classList.toggle('hoho');
+}
